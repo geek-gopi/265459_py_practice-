b720271dedd8a51bc7336767658b8fd53708725e
geek-gopi <mgopinath49@gmail.com>
2021-04-27 15:38:49 +0530

    coomit

--- Basic_operation/New_line_with_print_fun.py
+++ Basic_operation/New_line_with_print_fun.py
@@ -7,6 +7,8 @@ print("Down came the rain and washed the spider out.")
 
 print("\\") 
 
+# print("\") gives ---> SyntaxError: EOL while scanning string literal
+
 
 
 # comma is just used to pass multiple arguments still print() treats it same as single argument 
@@ -14,15 +16,25 @@ print("\\")
 print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
 
 
+#using end="/n" prints same as default print
+
+print("My name is", "Python.", end="\n")
+print("Monty Python.")
+
+
 #using end=" "
 
 print("My name is", "Python.", end=" ")
 print("Monty Python.")
 
-#using end="/n" prints same as default print
 
-print("My name is", "Python.", end="\n")
+#using end="" removing the space b/w "" will print as continuous line.
+
+print("My name is ", end="")
 print("Monty Python.")
+
+
+
 
 
 
---
+++ Basic_operation/seperation_using_print().py
@@ -0,0 +1,20 @@
+print("My", "name", "is", "Monty", "Python.", sep="/")
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+print("Name", "Gopinath","is in""Branch", "cse", sep="-->")
+
+print()
+print("Name", "Gopinath","is in","Branch", "cse", sep="-->")
+
+
+
+print("Name", "Gopinath","is in","Branch", "cse", sep=" ")
+
+#using end and sep together 
+
+print("My", "name", "is", sep="_", end="*")
+print("Gopi", "Nath.", sep="*", end="*\n")
+#trying out spacing
+
+print("My", "name", "is", sep="_", end=" *")
+print("Gopi", "Nath.", sep="*", end="*\n")
