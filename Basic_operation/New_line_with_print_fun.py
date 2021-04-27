8d17ab328236d5795abe50bcdd822a19f8eb1b7c
geek-gopi <mgopinath49@gmail.com>
2021-04-27 14:37:33 +0530

    Updating_with_new features

--- Basic_operation/New_line_with_print_fun.py
+++ Basic_operation/New_line_with_print_fun.py
@@ -1,3 +1,18 @@
 print("The itsy bitsy spider climbed up the waterspout.")
 print()
 print("Down came the rain and washed the spider out.")
+
+
+# TO print the "/" we use "//" coz "/" is escape sequence 
+
+print("\\") 
+
+
+
+# comma is just used to pass multiple arguments still print() treats it same  
+
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+
+
+
